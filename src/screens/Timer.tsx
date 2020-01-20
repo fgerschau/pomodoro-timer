00501d0e4ca8ccd65abf7699c903512e5406fc7e
Felix Gerschau <felix.gerschau@gmail.com>
2020-01-20 22:43:13 +0100

    Update main page styles

--- src/screens/Timer.tsx
+++ src/screens/Timer.tsx
@@ -55,12 +55,12 @@ const Timer: FC = observer(() => {
   useEffect(() => {
     setTimeout(() => {
       setShowExplanation(true);
-    }, 800);
+    }, 500);
   }, []);
 
   return (
     <div className={classes.wrapper}>
-      <Typography variant="h4" className={classes.header}>
+      <Typography variant="h2" className={classes.header}>
         Pomodoro Timer
       </Typography>
       <Typography variant="h1" className={classes.timer} data-test-id="timer-timeleft">
@@ -93,13 +93,13 @@ const Timer: FC = observer(() => {
           <hr />
           <div className={classes.explanation}>
             <Typography variant="h4">The process</Typography>
-            <Typography variant="body1" className={classes.explanationDetails}>
-              <p>1. Decide on the <Typography className={classes.strong} color="primary">task to be done.</Typography></p>
-              <p>2. Set the <Typography className={classes.strong} color="primary">pomodoro timer</Typography> (traditionally to 25 minutes). </p>
-              <p>3. <Typography className={classes.strong} color="primary">Work</Typography> on the task. </p>
-              <p>4. End work when the timer rings and <Typography className={classes.strong} color="primary">put a checkmark</Typography> on a piece of paper. </p>
-              <p>5. If you have fewer than four checkmarks, <Typography className={classes.strong} color="primary">take a short break (3 – 5 minutes)</Typography>, then go to step 2. </p>
-              <p>6. After four pomodoros, <Typography className={classes.strong} color="primary">take a longer break (15 – 30 minutes)</Typography>, reset your checkmark count to zero, then go to step 1. </p>
+            <Typography variant="body1" className={classes.explanationDetails} component="span">
+              <p>1. Decide on the <Typography className={classes.strong} color="primary" component="span">task to be done.</Typography></p>
+              <p>2. Set the <Typography className={classes.strong} color="primary" component="span">pomodoro timer</Typography> (traditionally to 25 minutes). </p>
+              <p>3. <Typography className={classes.strong} color="primary" component="span">Work</Typography> on the task. </p>
+              <p>4. End work when the timer rings and <Typography className={classes.strong} color="primary" component="span">put a checkmark</Typography> on a piece of paper. </p>
+              <p>5. If you have fewer than four checkmarks, <Typography className={classes.strong} color="primary" component="span">take a short break (3 – 5 minutes)</Typography>, then go to step 2. </p>
+              <p>6. After four pomodoros, <Typography className={classes.strong} color="primary" component="span">take a longer break (15 – 30 minutes)</Typography>, reset your checkmark count to zero, then go to step 1. </p>
             </Typography>
             <a href="https://en.wikipedia.org/wiki/Pomodoro_Technique" rel="noopener noreferrer" target="_blank">See the Wikipedia article</a>
           </div>
